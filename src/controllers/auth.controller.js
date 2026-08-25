6e08c052c4db6ccb02189cbce833e067ff6eff19
Angad Thakur <angadthakur2004@gmail.com>
2026-08-25 22:33:30 +0530

    Custom AsyncError Handler applied

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,42 +1,31 @@
-const { registerUser , loginUser} = require("../services/auth.service")
+const { registerUser , loginUser} = require("../services/auth.service");
+const asyncHandler = require("../utils/asyncHandler");
 
-const register = async (req,res) => {
-    try{
+const register = asyncHandler (async(req,res) => {
+    
         const result = await registerUser(req.body);
 
         res.status(201).json({
             success:true,
             ...result,
         });
-    }catch(error){
-       res.status(400).json({
-        success:false,
-        message :error.message,
-       });
-    }
-};
+});
 
-const login = async (req,res) => {
-    try{
+const login = asyncHandler (async (req,res) => {
+    
         const result = await loginUser(req.body);
 
         res.status(200).json({
             success :true,
             ...result,
         });
-    }catch(error){
-        res.status(401).json({
-            success : false,
-            message :error.message,
-        });
-    }
-};
+});
 
-const getMe = async (req,res) =>{
+const getMe = asyncHandler(async (req,res) =>{
     res.status(200).json({
         success: true,
         user: req.user,
     });
-}
+});
 
 module.exports = {register, login, getMe};
