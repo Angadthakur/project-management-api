4fee49ac3fb3ee6e19e68461ba881bf50ee98ea0
Angad Thakur <angadthakur2004@gmail.com>
2026-09-04 18:59:30 +0530

    Auth routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,14 +3,22 @@ const express = require("express");
 const {
     register , 
     login , 
-    getMe} = require("../controllers/auth.controller");
+    getMe
+} = require("../controllers/auth.controller");
 
 const protect = require("../middlewares/auth.middleware");
+const validate = require("../middlewares/validate.middleware");
+const { registerSchema, loginSchema } = require("../validators/auth.validator");
+const authorize = require("../middlewares/role.middleware");
+const rateLimiter = require("../middlewares/rateLimiter");
+
 
 const router = express.Router();
 
-router.post("/register",register);
-router.post("/login",login);
+router.post("/register", rateLimiter, validate(registerSchema),register);
+
+router.post("/login",validate(loginSchema),login);
+
 router.get("/me", protect,getMe);
 
 module.exports = router;
