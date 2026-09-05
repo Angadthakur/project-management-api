e27749b2bb07c6103f317cec7380b0a899760c88
Angad Thakur <angadthakur2004@gmail.com>
2026-09-05 22:03:17 +0530

    Update app.js configuration

--- src/app.js
+++ src/app.js
@@ -2,6 +2,12 @@ const express = require("express");
 
 const authRoutes = require("./routes/auth.routes");
 
+const errorHandler = require("./middlewares/error.middleware");
+
+const projectRoutes = require("./routes/project.routes")
+
+const taskRoutes = require("./routes/task.routes")
+
 const app = express();
 
 app.use(express.json());
@@ -14,5 +20,11 @@ app.get('/health' , (req,res) => {
 });
 
 app.use("/api/auth" , authRoutes);
+
+app.use("/api/projects", projectRoutes);
+
+app.use("/api/tasks", taskRoutes);
+
+app.use(errorHandler);
 
 module.exports = app;
