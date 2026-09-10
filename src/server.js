6a130cf442c86d9c423f90ee9a32224edca3aa90
Angad Thakur <angadthakur2004@gmail.com>
2026-09-10 20:09:12 +0530

    Updated the server.js file

--- src/server.js
+++ src/server.js
@@ -1,16 +1,29 @@
 require('dotenv').config();
 
 const app = require('./app');
+
 const connectDB  = require('./config/db');
+
+const redisClient = require("./config/redis");
 
 const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
-    await connectDB();
+    try{
+        await connectDB();
 
-    app.listen(PORT, ()=>{
-    console.log(`Server running on port : ${PORT}`);
-});
+        await redisClient.connect();
+
+        console.log("Redis connected");
+
+        app.listen(PORT ,() => {
+            console.log(`Server is running on port ${PORT}`);
+        })
+    }catch(error){
+        console.error(error);
+        process.exit(1);
+    }
+
 };
 
 startServer();
